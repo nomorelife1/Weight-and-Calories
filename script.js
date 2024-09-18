a9401c4cb3bf0faab92de698380d7cb9c8316a82
NoMoreLife <72035473+nomorelife1@users.noreply.github.com>
2024-09-19 02:29:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,24 @@ document.getElementById("weightForm").addEventListener("submit", function(event)
     document.getElementById("overweightLimit").textContent = "Overweight begins at: " + overweightLimit.toFixed(1) + " kg.";
     document.getElementById("obeseLimit").textContent = "Obesity starts at: " + obeseLimit.toFixed(1) + " kg.";
 });
+
+
+    document.addEventListener('contextmenu', function(event) {
+        event.preventDefault();
+    });
+
+
+    document.addEventListener('keydown', function(event) {
+
+        if (event.ctrlKey && event.key === 'u') {
+            event.preventDefault();
+        }
+
+        if (event.ctrlKey && event.shiftKey && event.key === 'i') {
+            event.preventDefault();
+        }
+
+        if (event.key === 'F12') {
+            event.preventDefault();
+        }
+    });
